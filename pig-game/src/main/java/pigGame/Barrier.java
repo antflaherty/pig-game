37d98931acc18ead3dbb5418958de321a9921b9e
Anthony Flaherty <aflaherty@financialforce.com>
2019-11-13 14:53:33 +0000

    Add a position to Barrier

--- pig-game/src/main/java/pigGame/Barrier.java
+++ pig-game/src/main/java/pigGame/Barrier.java
@@ -7,11 +7,14 @@ public class Barrier
 	public static final Color COLOR = new Color(0x000000);
 
     private int height;
-    private int width;
+	private int width;
+	
+	private Position topLeftCorner;
 
-    public Barrier(int width, int height)
+    public Barrier(int width, int height, Position topLeftCorner)
     {
         this.height = height;
-        this.width = width;
+		this.width = width;
+		this.topLeftCorner = topLeftCorner;
     }
 } 

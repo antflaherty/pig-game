5ae36feda1538a0928478045333f003a53d75724
Anthony Flaherty <aflaherty@financialforce.com>
2019-11-13 15:31:35 +0000

    Add target class

---
+++ pig-game/src/main/java/pigGame/Target.java
@@ -0,0 +1,28 @@
+package pigGame;
+
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+
+public class Target {
+
+	private Position position;
+	private Orientation orientation;
+	private BufferedImage sprite;
+	
+	public Target(Position position, Orientation orientation)
+	{
+		this.position = position;
+		this.orientation = orientation;
+		importSprite();
+	}
+
+	private void importSprite()
+	{
+		try {
+			sprite = ImageIO.read(getClass().getResource("/pig.png"));
+		} catch (Exception e) {
+			System.out.println("Error reading image file - " + e.toString());
+		}
+	}
+	
+}
